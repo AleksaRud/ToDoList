451d5b59222d00fcb00d242b718c8f115990eb6e
AleksaRud <alexandra.rudovich12@gmail.com>
2024-04-20 12:13:39 +0300

    some changes

--- ToDoList/src/components/DragDrop.vue
+++ ToDoList/src/components/DragDrop.vue
@@ -1,125 +1,139 @@
 <script lang="ts">
 import { ref } from 'vue';
 
-  export default {
+export default {
     setup() {
-      const items = ref([
-        { id: '0', title: 'Item A', list: 1, in_edit: false},
-        { id: '1', title: 'Item B', list: 1, in_edit: false},
-        { id: '2', title: 'Item C', list: 2, in_edit: false},
-        { id: '3', title: 'Item D', list: 3, in_edit: false},
-      ])
-      const getList = (list:number) => {
-       return items.value.filter((item) => item.list == list)
-      }
-
-      const startDrag = (event:DragEvent, item:{id:string, title:String, list:number, in_edit:boolean}) => {
-        console.log(item);
-
-        if(event.dataTransfer){
-          event.dataTransfer.dropEffect = 'move';
-        }
         
-        if(event.dataTransfer){
-          event.dataTransfer.effectAllowed = 'move';
-        }
-        if(event.dataTransfer){
-          event.dataTransfer.setData('itemID', item.id);
-        }
-      }
-
-      const onDrop = (event:DragEvent, list:number) => {
+        const items = ref([
+            { id: '0', title: 'Item A', list: 1, in_edit: false},
+            { id: '1', title: 'Item B', list: 1, in_edit: false},
+            { id: '2', title: 'Item C', list: 2, in_edit: false},
+            { id: '3', title: 'Item D', list: 3, in_edit: false},
+        ])
         
-        const itemID = event.dataTransfer ? event.dataTransfer.getData('itemID') : null;
-        if(itemID){
-          const item = items.value.find((item) => item.id == itemID);
-          if(item){
-            item.list = list;
-          }
+        localStorage.setItem('items', JSON.stringify(items));
+        const getList = (list:number) => {
+            return items.value.filter((item) => item.list == list);
+            
         }
-        
-      }
 
-      function addElement(list_id:number){
-        items.value.push({id: (items.value.length).toString(), title: 'new', list: list_id, in_edit: false});
-      }
+        const startDrag = (event:DragEvent, item:{id:string, title:String, list:number, in_edit:boolean}) => {
+            console.log(item);
 
-      function deleteElement(id:string){
-        let pos = items.value.findIndex(item => item.id == id);
-        items.value.splice(pos, 1);
-        for(let i=0; i<items.value.length; i++){
-          items.value[i].id = i.toString();
+            if(event.dataTransfer){
+                event.dataTransfer.dropEffect = 'move';
+            }
+            
+            if(event.dataTransfer){
+                event.dataTransfer.effectAllowed = 'move';
+            }
+            if(event.dataTransfer){
+                event.dataTransfer.setData('itemID', item.id);
+            }
         }
-      }
 
-      return {
-        getList,
-        onDrop,
-        startDrag,
-        addElement,
-        deleteElement,
-      }
+        const onDrop = (event:DragEvent, list:number) => {
+            
+            const itemID = event.dataTransfer ? event.dataTransfer.getData('itemID') : null;
+            if(itemID){
+                const item = items.value.find((item) => item.id == itemID);
+                if(item){
+                    item.list = list;
+                }
+            }
+            localStorage.setItem('items', JSON.stringify(items));
+        }
+
+        function addElement(list_id:number){
+            items.value.push({id: (items.value.length).toString(), title: 'new', list: list_id, in_edit: false});
+            localStorage.setItem('items', JSON.stringify(items));
+        }
+
+        function deleteElement(id:string){
+            let pos = items.value.findIndex(item => item.id == id);
+            items.value.splice(pos, 1);
+            for(let i=0; i<items.value.length; i++){
+                items.value[i].id = i.toString();
+            }
+            localStorage.setItem('items', JSON.stringify(items));
+        }
+
+        return {
+            getList,
+            onDrop,
+            startDrag,
+            addElement,
+            deleteElement,
+        }
     },
-  }
+}
 
 </script>
 
 <template>
     <div class="columns">
-        <div class="drop-zone" @drop="onDrop($event, 1)" @dragenter.prevent @dragover.prevent>
-            <div v-for="item in getList(1)" 
-            :key="item.id" 
-            class="drag-el" 
-            draggable="true" 
-            @dragstart="startDrag($event, item)">
-            <div v-if="!item.in_edit">
-              {{ item.title }}
-              <button @click="addElement(1)">+</button>
-              <button @click="deleteElement(item.id)">-</button>
-              <button @click="item.in_edit = !item.in_edit">edit</button>
-            </div>
-            <div v-if="item.in_edit">
-              <input type="text" v-model="item.title" />
-              <button @click="item.in_edit = !item.in_edit">ok</button>
-            </div>
-            </div>
-        </div>
-        <div class="drop-zone" @drop="onDrop($event, 2)" @dragenter.prevent @dragover.prevent>
-            <div v-for="item in getList(2)" 
-            :key="item.id" 
-            class="drag-el" 
-            draggable="true" 
-            @dragstart="startDrag($event, item)">
-            <div v-if="!item.in_edit">
-              {{ item.title }}
-              <button @click="addElement(2)">+</button>
-              <button @click="deleteElement(item.id)">-</button>
-              <button @click="item.in_edit = !item.in_edit">edit</button>
-            </div>
-            <div v-if="item.in_edit">
-              <input type="text" v-model="item.title" />
-              <button @click="item.in_edit = !item.in_edit">edit</button>
-            </div>
+        <div>
+            Не начато
+            <div class="drop-zone" @drop="onDrop($event, 1)" @dragenter.prevent @dragover.prevent>
+                <div v-for="item in getList(1)" 
+                :key="item.id" 
+                class="drag-el" 
+                draggable="true" 
+                @dragstart="startDrag($event, item)">
+                    <div v-if="!item.in_edit">
+                        {{ item.title }}
+                        <button @click="addElement(1)">+</button>
+                        <button @click="deleteElement(item.id)">-</button>
+                        <button @click="item.in_edit = !item.in_edit">edit</button>
+                    </div>
+                    <div v-if="item.in_edit">
+                        <input type="text" v-model="item.title" />
+                        <button @click="item.in_edit = !item.in_edit">ok</button>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="drop-zone" @drop="onDrop($event, 3)" @dragenter.prevent @dragover.prevent>
-            <div v-for="item in getList(3)" 
-            :key="item.id" 
-            class="drag-el" 
-            draggable="true" 
-            @dragstart="startDrag($event, item)">
-            <div v-if="!item.in_edit">
-              {{ item.title }}
-              <button @click="addElement(3)">+</button>
-              <button @click="deleteElement(item.id)">-</button>
-              <button @click="item.in_edit = !item.in_edit">edit</button>
+        <div>
+            В процессе
+            <div class="drop-zone" @drop="onDrop($event, 2)" @dragenter.prevent @dragover.prevent>
+                <div v-for="item in getList(2)" 
+                :key="item.id" 
+                class="drag-el" 
+                draggable="true" 
+                @dragstart="startDrag($event, item)">
+                    <div v-if="!item.in_edit">
+                        {{ item.title }}
+                        <button @click="addElement(2)">+</button>
+                        <button @click="deleteElement(item.id)">-</button>
+                        <button @click="item.in_edit = !item.in_edit">edit</button>
+                    </div>
+                    <div v-if="item.in_edit">
+                        <input type="text" v-model="item.title" />
+                        <button @click="item.in_edit = !item.in_edit">edit</button>
+                    </div>
+                </div>
             </div>
-            <div v-if="item.in_edit">
-              <input type="text" v-model="item.title" />
-              <button @click="item.in_edit = !item.in_edit">ok</button>
+        </div>
+        <div>
+            Готово
+            <div class="drop-zone" @drop="onDrop($event, 3)" @dragenter.prevent @dragover.prevent>
+                <div v-for="item in getList(3)" 
+                :key="item.id" 
+                class="drag-el" 
+                draggable="true" 
+                @dragstart="startDrag($event, item)">
+                    <div v-if="!item.in_edit">
+                        {{ item.title }}
+                        <button @click="addElement(3)">+</button>
+                        <button @click="deleteElement(item.id)">-</button>
+                        <button @click="item.in_edit = !item.in_edit">edit</button>
+                    </div>
+                    <div v-if="item.in_edit">
+                        <input type="text" v-model="item.title" />
+                        <button @click="item.in_edit = !item.in_edit">ok</button>
+                    </div>
+                </div>
             </div>
-            </div>
-            
         </div>
     </div>
 </template>
